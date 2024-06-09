a367e517faeece1be2e50eb6098a0af60cc5c030
Leng Lee <lenglee1@gmail.com>
2024-06-09 04:21:32 +0100

    Update app.py with new bucket name

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-import os
 from flask import Flask, request, redirect, url_for, render_template
 from google.cloud import storage
 
 app = Flask(__name__)
-storage_client = storage.Client()
-bucket_name = 'my-image-processing-bucket'  # Replace with your bucket name
-bucket = storage_client.bucket(bucket_name)
 
 @app.route('/')
 def index():
-    """Show the upload form and list of uploaded files."""
-    blobs = bucket.list_blobs()
-    return render_template('index.html', blobs=blobs)
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    """Handle the upload of a file."""
+def upload_image():
+    if 'file' not in request.files:
+        return 'No file part'
     file = request.files['file']
-    if file:
-        blob = bucket.blob(file.filename)
-        blob.upload_from_file(file)
-        return redirect(url_for('index'))
-    return "No file uploaded", 400
+    if file.filename == '':
+        return 'No selected file'
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('my-image-processing-bucket-us-central1')
+    blob = bucket.blob(file.filename)
+    blob.upload_from_file(file)
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
